c8a6fa2952b138990d828e86e0f071dae0481bfd
Joomy Korkut <joomy@cattheory.com>
2018-02-28 15:46:14 -0500

    Editorable interface resolution for Elab edit

--- src/Idris/Elab/Edit.hs
+++ src/Idris/Elab/Edit.hs
@@ -18,21 +18,31 @@ import Idris.Core.Elaborate
 import Idris.Core.TT
 import Idris.Core.Typecheck
 import Idris.Elab.Term
+import Idris.Elab.Value
 import Idris.Error
 import Idris.Reflection
 import Idris.SExp
 import Idris.Output
 
-namify :: String -> Idris Name
+namify :: Monad m => String -> m Name
 namify s = case reverse (splitOn "." s) of
   [] -> fail "Empty string"
   [x] -> return $ sUN x
   (x:xs) -> return $ sNS (sUN x) xs
 
+showUN :: Monad m => Name -> m T.Text
+showUN (UN x) = return x
+showUN (NS n xs) = showUN n
+showUN n = fail $ "Can't extract root name from " ++ show n
+
+tyInElab :: Monad m => Type -> m Type
+tyInElab (App _ (P _ n _) ty) | n == tacN "Elab" = return ty
+tyInElab x = fail $ "The term " ++ show x ++ " is not an Elab type"
+
 -- | Collect the non-arrow types in a given type.
 -- If the input is the syntax tree for @Bool -> Nat@, then the result should
 -- be a list of the syntax trees for @Bool@ and @Nat@.
-collectTypes :: Type -> Idris [Type]
+collectTypes :: Monad m => Type -> m [Type]
 collectTypes (Bind _ (Pi _ _ t1 _) t2) = (t1 :) <$> collectTypes t2
 collectTypes (Proj t _) = fail "Don't know how to collect Proj"
 collectTypes t = return [t]
@@ -48,9 +58,10 @@ elabEditAt fn nameStr l args =
      ist <- getIState
      name <- namify nameStr
      -- 1) lookup the name and type of the editor action
-     (ns, ty) <- case lookupTyNameExact name ctxt of
-                   Nothing -> fail "No such editor action"
-                   Just x -> return x
+     (tacTT, ty) <- elabVal toplevel ERHS (PRef NoFC [] name)
+     ns <- case tacTT of
+             P _ ns _ -> return ns
+             _ -> fail $ "The term " ++ show tacTT ++ " is not a variable reference"
      -- 2) check if the tactic has been declared "%editor"
      case lookupCtxtExact ns (idris_flags ist) of
        Nothing -> fail "The action is not declared anything"
@@ -66,16 +77,15 @@ elabEditAt fn nameStr l args =
      -- 4) call fromEditor for each of the SExps and get Terms
      --   * reflect Haskell SExps to Idris SExp ASTs
      rawArgs <- forM (zip collected args) $ \(argTy, sexp) -> do
-         let from = raw_apply (Var $ editN "fromEditor")
-                    [ forget argTy
-                    -- TODO 1: we don't know that the type is TT
-                    -- TODO 2: we don't know the namespace of the implementation yet,
-                    -- it might not be editNS
-                    , Var (editNS (SN $ ImplementationN (editN "Editorable") [T.pack "TT"]))
-                    , reflectSExp sexp]
-         (tm, maybeTy) <- tclift $ check ctxt [] from
+         (sexpTT, _) <- tclift $ check ctxt [] (reflectSExp sexp)
+         let sexpPTerm = delab ist sexpTT
+         let pterm = PApp NoFC (PRef NoFC [] (editN "fromEditor"))
+                               [ PImp 1 True [] (sUN "a") (delab ist argTy)
+                               , PExp 1 [] (sMN 0 "arg") sexpPTerm ]
+         (tm, _) <- elabVal toplevel ERHS pterm
          let tm' = normalise ctxt [] tm
-         (tm'', _) <- tclift $ elaborate "(toplevel)" ctxt emptyContext 0 (sMN 0 "editElab")
+         (tm'', _) <- tclift $ elaborate "(toplevel)" ctxt
+            (idris_datatypes ist) (idris_name ist) (sMN 0 "editElab")
             Erased initEState (reifyMaybe tm' >>=
               \case Just x -> return x ; _ -> fail "Nothing")
          return $ forget tm''
@@ -84,9 +94,12 @@ elabEditAt fn nameStr l args =
      let app = raw_apply (Var ns) rawArgs
      (app', _) <- tclift $ check ctxt [] app
     -- 6) run the tactic
-     (res, _) <- tclift $ elaborate "(toplevel)" ctxt emptyContext 0 (sMN 0 "editElab")
-            Erased initEState (runElabAction toplevel ist NoFC [] app' [] >>= reifyTT)
+     (res, _) <- tclift $ elaborate "(toplevel)" ctxt
+                   (idris_datatypes ist) (idris_name ist) (sMN 0 "editElab")
+                   Erased initEState
+                   (runElabAction toplevel ist NoFC [] app' [] >>= reifyTT)
             -- TODO fix reifyTT!!! It doesn't have to be TT. take toEditor into account!
+     lastTyInElab <- tyInElab (last collected)
      -- 7) delaborate into a PTerm
      -- iputStrLn $ show res
      let pterm = delabSugared ist (inlineSmall ctxt [] res)
